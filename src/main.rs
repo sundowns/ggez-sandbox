236f41688d5c9e5d9a205a1cfba453df948b04b3
Tom Smallridge <tom@evari.insure>
2019-08-30 17:06:53 +1000

    hello world

---
+++ src/main.rs
@@ -0,0 +1,60 @@
+use ggez::*;
+use nalgebra;
+
+// TODO: Lets try and use specs (an ECS): https://github.com/ggez/game-template
+
+fn main() {
+  // Make a Context and an EventLoop.
+  let (mut ctx, mut event_loop) = ContextBuilder::new("Some Dumb Game", "Tom Smallridge")
+    .build()
+    .unwrap();
+
+  // Create an instance of your event handler.
+  // Usually, you should provide it with the Context object
+  // so it can load resources like images during setup.
+  let mut my_game = DumbGame::new(&mut ctx);
+
+  // Run!
+  match event::run(&mut ctx, &mut event_loop, &mut my_game) {
+    Ok(_) => println!("Exited cleanly."),
+    Err(e) => println!("Error occured: {}", e),
+  }
+}
+
+struct DumbGame {
+  dt: std::time::Duration,
+  // font: graphics::Font,
+  text: graphics::Text,
+}
+
+impl DumbGame {
+  pub fn new(_ctx: &mut Context) -> DumbGame {
+    // Load/create resources here: images, fonts, sounds, etc.
+    DumbGame {
+      dt: std::time::Duration::new(0, 0),
+      text: graphics::Text::new(format!("dt: {}", 0)),
+    }
+  }
+}
+
+impl event::EventHandler for DumbGame {
+  fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
+    // Update code here...
+    self.dt = timer::delta(ctx);
+    self.text = graphics::Text::new(format!("dt: {:?}", self.dt));
+    Ok(())
+  }
+
+  fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+    graphics::clear(ctx, graphics::BLACK);
+
+    let my_dest = nalgebra::Point2::new(13.0, 37.0);
+    graphics::draw(
+      ctx,
+      &self.text,
+      graphics::DrawParam::default().dest(my_dest),
+    )?;
+
+    graphics::present(ctx)
+  }
+}
